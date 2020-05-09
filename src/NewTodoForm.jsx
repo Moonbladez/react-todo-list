d66633f5fe9c5674c92a0ab5104e8b9d5bb845ab
Bekki Young <moonbladez@hotmail.com>
2020-05-09 08:49:15 +0200

    intial commit

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class NewTodoForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { task: "" };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	//changes in the input
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
+
+	//submission of task
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props.createTodo(this.state);
+		this.setState({ task: "" });
+	}
+	render() {
+		return (
+			<form action='' onSubmit={this.handleSubmit}>
+				<label htmlFor='task'>New Task:</label>
+				<input
+					type='text'
+					placeholder='New Task'
+					id='task'
+					name='task'
+					value={this.state.task}
+					onChange={this.handleChange}
+				/>
+				<button>Add</button>
+			</form>
+		);
+	}
+}
+
+export default NewTodoForm;
